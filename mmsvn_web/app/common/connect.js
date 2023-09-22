28eb5d8a231c323e58097dadd0905474a0c173bc
Anhquan13 <anhquanmai655@gmail.com>
2023-09-22 23:31:41 +0700

    ket noi mysql online, chac hoi cham

--- mmsvn_web/app/common/connect.js
+++ mmsvn_web/app/common/connect.js
@@ -1,13 +1,14 @@
 var msql = require('mysql2');
 
 var conn = msql.createConnection({
-    host : 'localhost',
-    user : 'root',
+    host : '192.168.51.2',
+    port : '3306',
+    user : 'sa',
     password: '1234',
     database : 'mmsvn_db'
 })
 conn.connect(function (err){
-    if(err) console.log("Fail rồi mẹ nó đéo biết lỗi ở đâu luôn á");
+    if(err) console.log("Fail rồi  mẹ nó đéo biết lỗi ở đâu luôn á/n"+err);
     else console.log("Connected!");
 });
 
